3fe92463258a882db1de606b0955ddf98473cf03
Sebastian Uddén <sebastian.udden@gmail.com>
2025-03-14 14:05:19 +0100

    style tweaks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,24 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      keyframes: {
-        'fade-in-up': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(10px)'
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)'
-          },
-        }
+      fontFamily: {
+        audiowide: ["Audiowide", "cursive"],
       },
       animation: {
-        'fade-in-up': 'fade-in-up 0.3s ease-out'
-      }
+        "fade-in": "fadeIn 0.5s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
     },
   },
   plugins: [],
 };
 
-export default config; 
+export default config;
